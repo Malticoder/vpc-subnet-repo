9fde9d8ecea0b5d9ef31f9b8e6158ff504ff0f4f
maltidemo <150908958+Malticoder@users.noreply.github.com>
2025-09-02 20:58:01 +0530

    Update main.tf

--- terraform/environment/dev/main.tf
+++ terraform/environment/dev/main.tf
@@ -3,10 +3,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "vpc_subnet" {
-  source             = "../modules/vpc_subnet"
+  module "vpc_subnet" {
+  source             = "../../../modules/vpc_subnet"
   vpc_cidr           = var.vpc_cidr
   subnet_cidrs       = var.subnet_cidrs
   availability_zones = var.availability_zones
   environment        = "dev"
 }
+
+
