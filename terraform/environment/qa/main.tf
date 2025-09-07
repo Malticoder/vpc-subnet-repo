6eae322c0a2656e0a7c0393bfb94f70fbfc6d866
maltidemo <150908958+Malticoder@users.noreply.github.com>
2025-09-07 09:54:07 +0530

    Update main.tf

--- terraform/environment/qa/main.tf
+++ terraform/environment/qa/main.tf
@@ -6,10 +6,11 @@ provider "aws" {
 module "vpc_subnet" {
   source             = "../../modules/vpc_subnet"
   vpc_cidr           = "10.1.0.0/16"
-  subnet_cidrs       = var.subnet_cidrs
-  availability_zones = var.availability_zones
+  subnet_cidrs       = ["10.1.1.0/24", "10.1.2.0/24" ]
+  availability_zones = ["us-east-2a", "us-east-2b" ]
   environment        = "qa"
 }
+
 
 
 
